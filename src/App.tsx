220f6ce4e26385164446e4016f21b141ab14d03e
matheuxsx3 <matheus.s280604@gmail.com>
2023-11-14 16:57:55 -0300

    pronto

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,8 @@ import { AuthProvider } from './contexts/AuthContext';
 import ListaTemas from './components/temas/listaTemas/ListaTemas';
 import FormularioTema from './components/temas/formularioTema/FormularioTema';
 import DeletarTema from './components/temas/deletarTema/DeletarTema';
+import FormularioPostagem from './components/Postagens/formularioPostagem/FormularioPostagem';
+import ListaPostagens from './components/Postagens/listaPostagens/ListaPostagens';
 
 
 function App() {
@@ -29,6 +31,9 @@ function App() {
               <Route path="/cadastroTema" element={<FormularioTema />} />
               <Route path="/editarTema/:id" element={<FormularioTema />} />
               <Route path="/deletarTema/:id" element={<DeletarTema />} />
+              <Route path="/postagens" element={<ListaPostagens />} />
+              <Route path="/cadastroPostagem" element={<FormularioPostagem />} />
+              <Route path="/editarPostagem/:id" element={<FormularioPostagem />} />
             </Routes>
           </div>
           <Footer />
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,10 @@
 import homeLogo from '../../assets/home.png'
+import ListaPostagens from '../../components/Postagens/listaPostagens/ListaPostagens';
+import ModalPostagem from '../../components/Postagens/modalPostagem/ModalPostagem';
 import './Home.css';
+
+
+
 
 function Home() {
     return (
@@ -11,9 +16,9 @@ function Home() {
               <p className='text-xl'>Expresse aqui seus pensamentos e opniões</p>
   
               <div className="flex justify-around gap-4">
-              
-                <button className='rounded bg-white text-blue-800 py-2 px-4'>Ver postagens</button>
-              </div>
+              <ModalPostagem />
+              <button className='rounded bg-white text-blue-800 py-2 px-4'>Ver postagens</button>
+            </div>
             </div>
   
             <div className="flex justify-center ">
@@ -22,7 +27,7 @@ function Home() {
             </div>
           </div>
         </div>
-        
+        <ListaPostagens />
       </>
     );
 }

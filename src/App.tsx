e280e314b09a00f0d6f79b52d5fe775fcc0b9df6
matheuxsx3 <matheus.s280604@gmail.com>
2023-11-22 15:03:15 -0300

    Remove unused CSS import and update build script

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import './App.css';
-
 import Navbar from './components/navbar/Navbar';
 import Footer from './components/footer/Footer';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
